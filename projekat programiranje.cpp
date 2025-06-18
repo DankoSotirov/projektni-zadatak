d2073ef72df8dd2d98eac0e7129f6ea8b3908495
DankoSotirov <dankosotirov@gmail.com>
2025-06-18 13:21:25 +0200

    Update projekat programiranje.cpp

--- projekat programiranje.cpp
+++ projekat programiranje.cpp
@@ -20,20 +20,20 @@ void bfs(int startX, int startY) {
     queue<pair<int, int>> q;
     q.push({ startX, startY });
     visited[startX][startY] = true;
-    distanceMat[startX][startY] = 0;
+    distanceMat[startX][startY] = 0; //u red ubacuje startnu poziciju i upisuje 0 potrebnih koraka
 
     while (!q.empty()) {
-        auto [x, y] = q.front();
-        q.pop();
+        auto [x, y] = q.front(); //ispituje prvi element iz reda
+        q.pop();                 //izbacuje ga
 
-        for (int dir = 0; dir < 4; ++dir) {
+        for (int dir = 0; dir < 4; ++dir) {  //isprobava sve pravce
             int nx = x + dx[dir];
             int ny = y + dy[dir];
 
-            if (isValid(nx, ny)) {
-                visited[nx][ny] = true;
-                distanceMat[nx][ny] = distanceMat[x][y] + 1;
-                q.push({ nx, ny });
+            if (isValid(nx, ny)) {                              //ako je polje prohodno
+                visited[nx][ny] = true;                         //oznaci kao poseceno
+                distanceMat[nx][ny] = distanceMat[x][y] + 1;    //u matricu koraka upisi potreban broj koraka do tog polja
+                q.push({ nx, ny });                             //polje ubaci u red
             }
         }
     }
@@ -46,24 +46,24 @@ int main() {
         for (int j = 0; j < N; ++j) {
             int x;
             cin >> x;
-            if (x != 0 && x != 1) {
-                cout << "Greska: dozvoljeni su samo brojevi 0 i 1!" << endl;
+            if (x != 0 && x != 1) {                                    //ispitujemo da li je uneti broj 1 ili 0
+                cout << "Greska: dozvoljeni su samo brojevi 0 i 1!" << endl;   
                 return 1;
             }
             mat[i][j] = x;
         }
 
-    if (mat[0][0] == 1 || mat[N - 1][N - 1] == 1) {
+    if (mat[0][0] == 1 || mat[N - 1][N - 1] == 1) {                     //ispitujemo da li su pocetna i krajnja pozicija prohode
         cout << "Greska: pocetna i krajnja pozicija moraju biti 0 (prohodne)!" << endl;
         return 1;
     }
 
-    bfs(0, 0);
+    bfs(0, 0); //pozivamo bfs
 
     if (visited[N - 1][N - 1]) {
-        cout << "Najkraci broj koraka: " << distanceMat[N - 1][N - 1] << endl;
+        cout << "Najkraci broj koraka: " << distanceMat[N - 1][N - 1] << endl; //ako je bfs stigao do pozicije (N-1)(N-1) ispisi broj koraka
     } else {
-        cout << "Nema moguceg puta od (0,0) do (7,7)." << endl;
+        cout << "Nema moguceg puta od (0,0) do (7,7)." << endl; //ako nije.. nema moguceg resenja za unetu matricu
     }
 
     return 0;
